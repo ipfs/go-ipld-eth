4064f61ce28865054b675f36ac0733ba7307fb30
Herman Junge <chpdg42@gmail.com>
2017-07-20 23:01:47 -0400

    Add gx-go hook <function> to the osx hack

--- plugin/hacks/osx.sh
+++ plugin/hacks/osx.sh
@@ -16,6 +16,7 @@ set -e
 # Change
 echo "ipldeth github.com/ipfs/go-ipld-eth/plugin 0" >> $GOPATH/src/github.com/ipfs/go-ipfs/plugin/loader/preload_list
 sed -i '' 's/package main/package plugin/' $GOPATH/src/github.com/ipfs/go-ipld-eth/plugin/eth.go
+gx-go hook pre-test
 
 # Build and Execute
 cd $GOPATH/src/github.com/ipfs/go-ipfs/
@@ -25,4 +26,6 @@ make build
 # Revert
 git checkout -- $GOPATH/src/github.com/ipfs/go-ipfs/plugin/loader/preload_list
 git checkout -- $GOPATH/src/github.com/ipfs/go-ipfs/plugin/loader/preload.go
+cd $GOPATH/src/github.com/ipfs/go-ipld-eth
+gx-go hook post-test
 sed -i '' 's/package plugin/package main/' $GOPATH/src/github.com/ipfs/go-ipld-eth/plugin/eth.go
