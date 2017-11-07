b857918928efe41e3ef59a7a3a1be336f31fa991
Herman Junge <chpdg42@gmail.com>
2017-11-07 02:31:54 -0300

    Use better gx-go commands (rw and uw)

--- plugin/hacks/osx.sh
+++ plugin/hacks/osx.sh
@@ -16,7 +16,7 @@ set -e
 # Change
 echo "ipldeth github.com/ipfs/go-ipld-eth/plugin 0" >> $GOPATH/src/github.com/ipfs/go-ipfs/plugin/loader/preload_list
 sed -i '' 's/package main/package plugin/' $GOPATH/src/github.com/ipfs/go-ipld-eth/plugin/eth.go
-gx-go hook pre-test
+gx-go rw
 
 # Build and Execute
 cd $GOPATH/src/github.com/ipfs/go-ipfs/
@@ -27,5 +27,5 @@ make build
 git checkout -- $GOPATH/src/github.com/ipfs/go-ipfs/plugin/loader/preload_list
 git checkout -- $GOPATH/src/github.com/ipfs/go-ipfs/plugin/loader/preload.go
 cd $GOPATH/src/github.com/ipfs/go-ipld-eth
-gx-go hook post-test
+gx-go uw
 sed -i '' 's/package plugin/package main/' $GOPATH/src/github.com/ipfs/go-ipld-eth/plugin/eth.go
